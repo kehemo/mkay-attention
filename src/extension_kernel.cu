e7a14f80ed637dd9b30b212f32feccad606da56b
Anugrah Chemparathy <anugrahchemparathy@gmail.com>
2024-11-30 19:38:59 -0800

    basic reorganization of starter code since kenneth's starter code commit

--- src/extension_kernel.cu
+++ src/extension_kernel.cu
@@ -27,43 +27,46 @@ __global__ void add_one_kernel(float *data, int size)
     }
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+///  YOU DO NOT NEED TO MODIFY THE CODE ABOVE HERE (unless you want to).     ///
+////////////////////////////////////////////////////////////////////////////////
+
+
+
 /// <--- your code here --->
 
 __global__ void attention_forward(const num *q, const num *k, const num *v, num *o, int batch_size, int nheads, int seqlen, int head_dim)
 {
 }
 
-/// <--- /your code here --->
 
-////////////////////////////////////////////////////////////////////////////////
-///          YOU DO NOT NEED TO MODIFY THE CODE BELOW HERE.                  ///
-////////////////////////////////////////////////////////////////////////////////
 std::vector<torch::Tensor> launch_attention_forward(torch::Tensor q, torch::Tensor k, torch::Tensor v)
 {
     int batch_size = q.size(0);
     int seqlen = q.size(1);
     int nheads = q.size(2);
     int head_dim = q.size(3);
+
+    // malloc output array
     num *o;
     CUDA_CHECK(cudaMalloc(&o, batch_size * seqlen * nheads * head_dim * sizeof(num)));
-    int blocks = 1;
-    int threads = 1;
-    attention_forward<<<blocks, threads>>>(q.data_ptr<num>(), k.data_ptr<num>(), v.data_ptr<num>(), o, batch_size, nheads, seqlen, head_dim);
+
+    // launch your kernels
+    attention_forward<<<1, 1>>>(q.data_ptr<num>(), k.data_ptr<num>(), v.data_ptr<num>(), o, batch_size, nheads, seqlen, head_dim);
+    // cudaDeviceSynchronize();
+    // in case you want multiple kernels
+
+    // Create a tensor from the output array
     auto options = torch::TensorOptions().device(torch::kCUDA).dtype(torch::kBFloat16);
     auto o_tensor = torch::from_blob(o, {batch_size, seqlen, nheads, head_dim}, options);
     return {o_tensor};
 }
 
-// Wrapper function.
-std::vector<torch::Tensor> my_cuda_function(torch::Tensor input_tensor)
-{
-    auto data = input_tensor.data_ptr<float>();
-    auto size = input_tensor.numel();
 
-    int threads = 1024;
-    int blocks = (size + threads - 1) / threads;
-    add_one_kernel<<<blocks, threads>>>(data, size);
-    cudaDeviceSynchronize();
 
-    return {input_tensor};
-}
+/// <--- /your code here --->
+
+////////////////////////////////////////////////////////////////////////////////
+///          YOU DO NOT NEED TO MODIFY THE CODE BELOW HERE.                  ///
+////////////////////////////////////////////////////////////////////////////////
