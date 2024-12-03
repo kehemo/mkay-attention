bdefca1cea8743dd8da0452852a7fca2b4a655f2
martinch <martinch@mit.edu>
2024-12-03 00:57:42 -0500

    (WIP) Commit in-progress code for backwards pass (currently incomplete)

--- src/check.py
+++ src/check.py
@@ -1,3 +1,4 @@
+from numpy import ceil
 import torch
 import os
 import math
@@ -5,8 +6,21 @@ from einops import rearrange
 from ctypes import *
 import sys
 
+"""
+Backward Pass Implementation Strategy:
+- Very liable to messing up a first pass correct implementation of the backward pass.
+    - Also kind of tricky dealing with the batching and the multiple heads.
+- To help debug, we can use a hierarchy of slightly simpler implementations that can 
+  be tested against simpler ones (e.g., by comparing intermediate values), until we
+  have one that can be tested directly against the `.backward(g)` Pytorch builtin.
 
-def attention_torch_checkpoint(qkv):
+Hierarchy of implementations
+- 
+
+"""
+sram_size_bytes = 100_000
+
+def fwd_reference(qkv):
     attn_dtype, attn_device = qkv.dtype, qkv.device
 
     batch_size, seqlen, _, nheads, d = qkv.shape
@@ -18,7 +32,104 @@ def attention_torch_checkpoint(qkv):
     output = torch.einsum("b h t s, b s h d -> b t h d", attention, v)
     # output = attention
 
-    return output.to(dtype=qkv.dtype)
+    # O, S, P
+    # O: b t h d
+    # S: b h t s (but s is pre-softmax)
+    # P: b h t s
+    return output.to(dtype=qkv.dtype), attn_scores, attention
+
+def attention_torch_checkpoint(qkv):
+    output, *_ = fwd_reference(qkv)
+    return output
+
+# Pytorch implementation of the naive backward pass.
+# - can *test against* the .backward() of the fwd reference.
+# - can *be tested against* for the Pytorch implementation of the backward pass 
+def bwd_expanded(
+        qkv: torch.Tensor,
+        o: torch.Tensor,
+        s: torch.Tensor,
+        p: torch.Tensor,
+        g: torch.Tensor):
+    qkv = qkv.detach().clone().requires_grad_(True)
+    o, s, p = fwd_reference(qkv)
+    q, k, v = qkv.unbind(dim=2)
+    
+    dV = p.T @ g  # g is dO
+    dP = g @ v.T
+
+    # Calculate ds
+    # Subtraction factor
+
+    # Einsums my arch nemesis ✊✊✊✊
+    dSf = torch.einsum("-> ", p, dP)
+    dS = torch.einsum("", )
+    raise NotImplementedError
+
+def test_bwd_expanded(qkv: torch.Tensor):
+    qkv = qkv.detach().clone().requires_grad_(True)
+
+    o, s, p = fwd_reference(qkv)  # Technically a waste of computation.
+    g = torch.randn_like(o)  # Fake loss function
+    qkv.backward(g)
+
+    ref = qkv.grad
+
+    # Note for later versions we can also start extracting intermediate results.
+    expanded = bwd_expanded(qkv, o, s, p, g)
+
+    get_tensor_difference(ref, expanded)
+    rel_rmse = compute_relative_rmse(ref, expanded)
+    print(f"\n\n>>> Relative RMSE: {rel_rmse}")
+
+# Pytorch implementation of the FlashAttention backward pass.
+# - can *test against* the `bwd_expanded` with intermediate values.
+# - can *be tested against* for the CUDA implementation of the backward pass
+def bwd_flash(qkv: torch.Tensor):
+    batch_size, seqlen, _, nheads, d = qkv.shape
+    q, k, v = qkv.unbind(dim=2)
+
+    l, m = 0, 0
+    B_c = ceil(sram_size_bytes / (4 * d))
+    B_r = min(B_c, d)
+
+    T_r = ceil(seqlen / B_r)  # Not affected by num heads or batch_size
+    T_c = ceil(seqlen / B_c)
+
+    assert seqlen == T_r * B_r, f"N ({seqlen}) must equal T_r * B_r ({T_r * B_r})"
+
+    Qs = rearrange(q, 'b (t r) h d -> t b r h d', t=T_r, r=B_r)
+    Ks = rearrange(k, 'b (t c) h d -> t b c h d', t=T_c, r=B_c)
+    Vs = rearrange(v, 'b (t c) h d -> t b c h d', t=T_c, r=B_c)
+
+    # Step 5
+    dqs = torch.zeros_like(Qs)
+    dks = torch.zeros_like(Ks)
+    dvs = torch.zeros_like(Vs)
+
+    for j in range(T_c):
+        dKw = torch.zeros_like(Ks) # I don't know convention for the swiggle on top
+        dVw = torch.zeros_like(Vs)
+        for j in range(T_r):
+            s = torch.einsum("")
+
+    raise NotImplementedError
+
+# First pass at the backward pass, in Pytorch first.
+# This is deterministic. If we were to do dropout and stuff, we'd the p_drop and rand_state too.
+def attention_flash_attn_bwd(qkv, o, do, l, m):
+    """
+    Inputs:
+        QKV in their usual shape, N x d
+        O, our ouput              N x d
+        dO, our "output gradient" N x d (But this is actually simulated)
+        l, statistics             N
+        m, statistics             N
+    """
+    batch_size, seqlen, _, nheads, d = qkv.shape
+    q, k, v = qkv.unbind(dim=2)
+    softmax_scale = 1.0 / math.sqrt(d)
+    raise NotImplementedError
 
 
 def get_tensor_difference(t1, t2):
